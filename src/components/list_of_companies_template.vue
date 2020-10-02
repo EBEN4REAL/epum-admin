e19587e734521bacc2bbc48d5c3b77c212334cb9
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-10-02 11:36:50 +0100

    ..

--- src/components/list_of_companies_template.vue
+++ src/components/list_of_companies_template.vue
@@ -1,20 +1,20 @@
 <template>
    <div class="btn-group btn-group-sm" role="group">
           <router-link v-b-tooltip.hover title="Edit" :to="{name:'edit_companies'}" class="btn btn-warning" >
-                <i class="fa fa-map-o mr-1" aria-hidden="true"></i>
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
           </router-link> 
            <router-link v-b-tooltip.hover title="Details" :to="{name:'company_details'}" class="btn btn-primary" >
-                <i class="fa fa-refresh mr-1" aria-hidden="true"></i>
+                <i class="fa fa-info-circle" aria-hidden="true"></i>
           </router-link> 
            <router-link v-b-tooltip.hover title="Branches" :to="{name:'map_user_sales_rep'}" class="btn btn-info" >
-                <i class="fa fa-map-o mr-1" aria-hidden="true"></i>
+                <i class="fa fa-building-o" aria-hidden="true"></i>
           </router-link> 
             <router-link v-b-tooltip.hover title="Sales Rep" :to="{name:'map_user_to_role'}" class="btn btn-primary" >
-                <i class="fa fa-refresh mr-1" aria-hidden="true"></i>
+                <i class="fa fa-mars" aria-hidden="true"></i>
           </router-link>
 
            <router-link v-b-tooltip.hover title="Mail Recipient" :to="{name:'map_user_to_comapny'}" class="btn btn-success" >
-                <i class="fa fa-refresh mr-1" aria-hidden="true"></i>
+                <i class="fa fa-reply" aria-hidden="true"></i>
           </router-link>
   </div>
 </template>
