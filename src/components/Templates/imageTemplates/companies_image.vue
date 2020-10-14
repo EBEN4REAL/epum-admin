05d92d287b653e5c815e0481aec09d1227101065
favour-chibueze <chibueze.favour.o@gmail.com>
2020-10-14 11:36:00 +0100

    update

---
+++ src/components/Templates/imageTemplates/companies_image.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+       <img src="@/assets/img/company_icon.png" alt="" class="img-responsive" width="30">
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        data: {}
+    }
+  },
+  mounted() {
+  },
+}
+</script>
