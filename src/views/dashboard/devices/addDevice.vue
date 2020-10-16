42e3b2a334ea75d7106701d15bc083a7638c946d
Omoruyis <omoruyi.isaac97@gmail.com>
2020-10-16 15:20:09 +0100

    update

--- src/views/dashboard/devices/addDevice.vue
+++ src/views/dashboard/devices/addDevice.vue
@@ -26,16 +26,15 @@
               </div>
               <div class="col-md-8">
                 <div class="input__block">
-                  <select class="form-control" >
-                      <option disabled selected>Select Device</option>
-                      <option>Pump</option>
-                      <option>Tank</option>
-                      <option>POS</option>
+                  <select class="form-control" v-model="selectedDevice">
+                      <option disabled selected value="selectADevice">Select Device</option>
+                      <option value="Pump">Pump</option>
+                      <option value="Tank">Tank</option>
                   </select>
                 </div>
               </div>
             </div>
-            <div class="row  mt-3">
+            <!-- <div class="row  mt-3">
               <div class="col-md-4 text-left">
                 <label>Pumps</label>
               </div>
@@ -60,6 +59,19 @@
                  P2
                 </div>
               </div>
+            </div> -->
+            <div class="row align-items-center mt-3" v-if="selectedDevice !== 'selectADevice'">
+              <div class="col-md-4 text-left">
+                <label>{{selectedDevice}}</label>
+              </div>
+              <div class="col-md-8">
+                <div class="input__block">
+                  <select class="form-control" v-model="deviceId">
+                      <option disabled selected value="selectADevice">Select A {{selectedDevice}}</option>
+                      <option v-for="(device, index) in selectedDevice == 'Tank' ? tanks : pumps" :key="index" :value="device.id">{{device.name}}</option>
+                  </select>
+                </div>
+              </div>
             </div>
             <div class="row align-items-center mt-3">
               <div class="col-md-4 text-left">
@@ -67,7 +79,7 @@
               </div>
               <div class="col-md-8">
                 <div class="input__block">
-                  <input type="tel" placeholder="Phone Number on Device" class="" />
+                  <input type="text" placeholder="Phone Number on Device" class="" v-model="phoneNumber"/>
                 </div>
               </div>
             </div>
@@ -77,7 +89,7 @@
               </div>
               <div class="col-md-8">
                 <div class="input__block">
-                  <input type="text" placeholder="" class="" />
+                  <input type="text" placeholder="" class="" v-model="inputDeviceId"/>
                 </div>
               </div>
             </div>
@@ -87,11 +99,25 @@
               </div>
               <div class="col-md-8 text-left">
                <div class="form-check form-check-inline">
-                 <input class="form-check-input" type="checkbox" id="" value="Device is Demo">
+                 <input class="form-check-input" type="checkbox" id=""  @change="toggleIsDemo">
                  Device is Demo
                 </div>
                 <div class="text-center mt-3">
-                  <button class="btn btn_theme">Create</button>
+                  <button class="btn btn-success" @click="addDevice"
+                      :disabled="isButtonDisabled ? true : null"
+                      :style="[
+                        isButtonDisabled
+                          ? { cursor: 'not-allowed' }
+                          : { cursor: 'pointer' }
+                      ]"
+                    >Add Device
+                      <img
+                        src="@/assets/img/git_loader.gif"
+                        style="display:none"
+                        width="35px"
+                        class="ml-3 loader"
+                      />
+                  </button>
                 </div>
               </div>
             </div>
@@ -106,6 +132,9 @@
 import Vue from "vue";
 import masterLayout from "@/views/dashboard/masterLayout";
 import backgroundUrl from "@/assets/img/bg__card.png";
+import configObject from "@/config";
+import Jquery from 'jquery';
+let $ = Jquery;
 
 export default {
   components: {
@@ -116,10 +145,20 @@ export default {
   data() {
     return {
       backgroundUrl,
-      comapanyBranchObj: {}
+      comapanyBranchObj: {},
+      selectedDevice: 'selectADevice',
+      deviceId: 'selectADevice',
+      pumps: [], 
+      tanks: [],
+      isDemo: false,
+      phoneNumber: null,
+      inputDeviceId: null,
+      isButtonDisabled: false
     };
   },
   mounted() {
+    this.getTanks()
+    this.getProbes()
     this.companyBranchId = this.$route.query.companyBranchId
     let ml = sessionStorage.getItem(this.companyBranchId)
     if (!ml){
@@ -134,7 +173,114 @@ export default {
     }
     let companyBranchDetails = JSON.parse(ml)
     this.comapanyBranchObj = companyBranchDetails
-    console.log(this.comapanyBranchObj)
+  },
+  methods: {
+    toggleIsDemo() {
+      this.isDemo = !this.isDemo
+    },
+    getTanks() {
+        this.axios
+        .get(
+        `${configObject.apiBaseUrl}/Branch/Tanks/${this.$route.query.companyBranchId}`,
+        configObject.authConfig
+        )
+        .then(response => {
+            this.tanks = response.data
+        })
+        .catch(error => {});
+    },
+    // getProbes() { // for getting probetransactions
+    //     this.axios
+    //     .get(
+    //     `${configObject.apiBaseUrl}/Tank/ProbeTransactions/8c903156-fd5f-494b-8b97-5e55e417f7dd/?startDate=2020-10-16T11:28:17.791Z&endDate=2020-10-16T11:28:17.791Z`,
+    //     configObject.authConfig
+    //     )
+    //     .then(response => {
+    //         console.log(response.data)
+    //     })
+    //     .catch(error => {
+    //       console.log(error.response)
+    //     });
+    // },
+    addDevice(event) {
+      event.preventDefault();
+      if(this.selectedDevice == 'selectADevice') {
+          this.$toast("Please select a device", {
+              type: "error", 
+              timeout: 3000
+          });
+          return;
+      }
+
+      if(this.deviceId == 'selectADevice') {
+          this.$toast(`Please select a ${this.selectedDevice}`, {
+              type: "error", 
+              timeout: 3000
+          });
+          return;
+      }
+      
+      if(!this.phoneNumber) {
+          this.$toast("Please input a phone number", {
+              type: "error", 
+              timeout: 3000
+          });
+          return;
+      }
+
+      if(this.phoneNumber.length < 10 || this.phoneNumber.length > 13) {
+          this.$toast("Please input a phone number with 10 to 13 digits", {
+              type: "error", 
+              timeout: 3000
+          });
+          return;
+      }
+
+      if(!this.inputDeviceId) {
+          this.$toast("Please input a phone number", {
+              type: "error", 
+              timeout: 3000
+          });
+          return;
+      }
+      
+      const data = {
+          branchId: this.$route.query.companyBranchId,
+          deviceId: this.inputDeviceId,
+          isDemo: this.isDemo,
+          deviceType: this.selectedDevice,
+          pumpId: this.selectedDevice == 'Pump' ? this.deviceId : '',
+          phoneNumber: this.phoneNumber,
+          tankId: this.selectedDevice == 'Tank' ? this.deviceId : '',
+          probeAddress: 'string',
+          userID: 'string',
+          devicePass: 'string'
+      }
+
+      console.log(data);
+
+      $('.loader').show();
+      this.isButtonDisabled = true;
+
+       this.axios.post(`${configObject.apiBaseUrl}/Company/AddDevice`,data, configObject.authConfig)
+          .then(res => {
+                this.$toast("Successfully Added Device", {
+                    type: "success",
+                    timeout: 3000
+                });
+                this.isButtonDisabled = false;
+                $('.loader').hide();
+                // this.$router.push({name: 'branches', query: {companyId: this.$route.query.companyId}})
+          })
+          .catch(error => {
+              this.isButtonDisabled = false;
+              $('.loader').hide();
+              this.$toast(error.response.data.message, {
+                  type: "error",
+                  timeout: 3000
+              });
+          });
+    }
   }
 };
 </script>
