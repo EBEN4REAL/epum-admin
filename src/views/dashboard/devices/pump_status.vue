4df25db725584c5e08fc3f67042aa8d9e1b6e5c3
favour-chibueze <chibueze.favour.o@gmail.com>
2020-10-22 11:58:06 +0100

    update

--- src/views/dashboard/devices/pump_status.vue
+++ src/views/dashboard/devices/pump_status.vue
@@ -3,34 +3,39 @@
         <section class=" mt-3 full__row_section">
             <div class="banner">
             <div class="row">
-                <div class="col-lg-8 remove-padding-left padding_div pr-0">
-                    <div class="dashboard__card small_card align-center">
-                        <div class="row">
-                        <div class="col-md-8 card_inner_wrapper">
-                            <h3>Hi, {{userName}}</h3>
-                            <p>Get started with epump company admin platform by managing your pumps here</p>
-                        </div>
-                        <div class="col-md-4 mt-4 text-center">
-                           
+                <div class="col-lg-12">
+                        <div class="dashboard__card large_card"  :style="[
+                                    {
+                                    backgroundImage: `linear-gradient(rgb(37, 37, 37 , 0.9), rgb(37, 37, 37 , 0.9)), url(${backgroundUrl})`,
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover'
+                                    }
+                                ]">
+                                <div class="row align-items-center justify-content-center hundred-percent-height">
+                                     <div class="col-md-2">
+                                    <small class="text-white">Refresh in
+                                      0:00</small>
+                                    </div>
+                                    <div class="col-md-4">
+                                         <div class="small__card_content_wrapper align-items-center " >
+                                            <p class="dashboard__card__header text-white">PUMP STATUS</p>
+                                        </div>
+                                    </div>
+                                <label class="radio-inline text-white mr-4"><input type="radio" >Not Okay</label>
+                                <label class="radio-inline text-white"><input type="radio">Show All</label>
+                                    <div class="col-md-3">
+                                         <div class="search-container">
+                                            <form action="">
+                                            <input type="text" placeholder="Search.." name="search" class="input__block">
+                                            <button type="submit" class="search_button"><i class="fa fa-search"></i></button>
+                                            </form>
+                                    </div>
+                                </div>
                         </div>
                     </div>
-                </div>
-             </div>
-                 <div class="col-lg-4">
-                    <div class="dashboard__card large_card">
-                        <div class="small__card_content_wrapper align-items-center justify-content-center" >
-                            <p class="dashboard__card__header text-white">Pump Status</p>
-                                <div class="icon_wrapper centralize text-center" style="margin-top: -12px;">
-                                <img src="@/assets/img/company.png" width="40px" />
-                                </div>
-                                <div class="">
-                                <small class="dashboard__card__header_bottom text-white font-weight-bold"
-                                >7</small>
-                                </div>
-                        </div>
-                    </div>
-              </div>
             </div>
+        </div>
         </div>
         </section>
         <div class="new_row_section mt-3">
