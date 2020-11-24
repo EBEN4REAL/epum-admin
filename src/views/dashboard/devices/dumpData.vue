b340e81403e516b69754ba3061c3132532655d04
favour-chibueze <chibueze.favour.o@gmail.com>
2020-11-24 11:46:27 +0100

    updated

--- src/views/dashboard/devices/dumpData.vue
+++ src/views/dashboard/devices/dumpData.vue
@@ -5,23 +5,9 @@
     <div class="row">
       <div class="col-lg-7 col-md-6">
         <div class="new_row_section">
-          <div class="row mt-3">
+          <div class="row">
             <div class="col-lg-12 remove-padding-left padding_div pr-0">
-                    <div class="dashboard__card small_card align-center">
-                        <div class="row">
-                        <div class="col-md-8 card_inner_wrapper">
-                            <h3>Hi, {{userName}}</h3>
-                            <p>Get started with epump company admin platform by getting to manage your dumped data</p>
-                        </div>
-                        <div class="col-md-4 text-center">
-                            <img src="@/assets/img/audit.svg" class="mt-2" width="50%" />
-                        </div>
-                    </div>
-                </div>
-             </div>
-            </div>
-
-          <div class="small_card product_details_card audit-sales dumpdata_card mt-3">
+              <div class="small_card product_details_card audit-sales dumpdata_card">
               <div class="row p-4 align-items-center">
             <div class="col-md-4">
             <div class="input__block input_margin">
@@ -61,12 +47,13 @@
                       />
                     </button>
                 </div>
-            </div>
-
+              </div>
             </div>
           </div>
         </div>
-      </div>
+    </div>
+ </div>
+</div>
       <div class="col-lg-5 col-md-6" style="position: relative">
         <div class="small_card product_details_card audit-sales dumped_data mt-3">
               <div class="row p-4 align-items-center">
@@ -101,11 +88,13 @@
             </div>
             </div>
           </div>
-           <button class="mt-4 btn btn-success text-white togled_all_ddata" @click="toggleAll"
-          > Toggle All Data
-          </button>
       </div>
     </div>
+    <div class="toggler-button">
+             <button class="mt-4 btn btn-success text-white ml-3" @click="toggleAll"> 
+               Toggle All Data
+          </button>
+           </div>
     <div class="new_row_section mt-3">
           <ejs-grid
             v-show="!showLoader"
@@ -118,10 +107,9 @@
             :rowDataBound='rowDataBound'
             >
             <e-columns>
-                <e-column width="80" field="index" headerText="#"></e-column>
-                <e-column width="100" field="date" headerText="Date"></e-column>
-                <e-column width="400" field="string" headerText="DData"></e-column>
-                <e-column width="400" field="dData" headerText="DData" :visible="false"></e-column>
+                <e-column width="100" field="date" headerText="Date" text-align="left"></e-column>
+                <e-column width="400" field="string" headerText="DData" text-align="left"></e-column>
+                <e-column width="400" field="dData" headerText="DData" :visible="false" text-align="left"></e-column>
                 <e-column width="80" headerText="Toggle Dump Data"  :template="toggleDumpData"></e-column>
 
             </e-columns>
@@ -227,8 +215,8 @@ export default {
       this.$refs.dataGrid.refresh();
     },
     toggleAll() {
-      this.$refs.dataGrid.getColumns()[3].visible = !this.$refs.dataGrid.getColumns()[3].visible
       this.$refs.dataGrid.getColumns()[2].visible = !this.$refs.dataGrid.getColumns()[2].visible
+      this.$refs.dataGrid.getColumns()[1].visible = !this.$refs.dataGrid.getColumns()[1].visible
       this.refreshGrid();
     },
     rowDataBound(arging){
@@ -236,14 +224,14 @@ export default {
         if (!(args.target.classList.contains('fa-eye') || args.target.classList.contains('fa-eye-slash') || args.target.classList.contains('eye_holder'))) {
           return
         }
-        if(arging.row.children[2].innerHTML == arging.data.dData) {
-          arging.row.children[2].innerHTML = arging.data.string
-          arging.row.children[3].innerHTML = arging.data.dData
-          arging.row.children[4].innerHTML = '<div><button class="text-center var_btn eye_holder"><!----><i class="fa fa-eye"></i></button></div>'
-        }else {
+        if(arging.row.children[1].innerHTML == arging.data.dData) {
+          arging.row.children[1].innerHTML = arging.data.string
           arging.row.children[2].innerHTML = arging.data.dData
-          arging.row.children[3].innerHTML = arging.data.string
-          arging.row.children[4].innerHTML = '<div><button class="text-center var_btn eye_holder"><!----><i class="fa fa-eye-slash "></i></button></div>'
+          arging.row.children[3].innerHTML = '<div><button class="text-center var_btn eye_holder"><!----><i class="fa fa-eye"></i></button></div>'
+        }else {
+          arging.row.children[1].innerHTML = arging.data.dData
+          arging.row.children[2].innerHTML = arging.data.string
+          arging.row.children[3].innerHTML = '<div><button class="text-center var_btn eye_holder"><!----><i class="fa fa-eye-slash "></i></button></div>'
         }
       });
   },
